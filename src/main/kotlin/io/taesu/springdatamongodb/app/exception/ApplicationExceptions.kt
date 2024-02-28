195edd3ed0e2e8e5a33c22f502eb0c71b934dd97
taesu <taesu@crscube.co.kr>
2024-02-28 23:41:20 +0900

    Spring Webmvc 설정
    예외 계층 추가

---
+++ src/main/kotlin/io/taesu/springdatamongodb/app/exception/ApplicationExceptions.kt
@@ -0,0 +1,16 @@
+package io.taesu.springdatamongodb.app.exception
+
+/**
+ * Created by itaesu on 2024/02/28.
+ *
+ * @author Lee Tae Su
+ * @version spring-data-mongodb
+ * @since spring-data-mongodb
+ */
+open class BusinessException(val errorCode: ErrorCode): RuntimeException()
+
+class ResourceNotFoundException(val identifier: String): BusinessException(ErrorCode.RESOURCE_NOT_FOUND)
+
+class UnexpectedException: RuntimeException() {
+    val errorCode: ErrorCode = ErrorCode.UNEXPECTED
+}
